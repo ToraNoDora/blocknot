eb3eb723e0e81481d07296f4d30fc51ea1ef6f09
ToraNoDora <toranodora@yandex.ru>
2021-02-15 17:11:55 +0300

    add table

--- main.py
+++ main.py
@@ -2,55 +2,54 @@ import sys
 import os
 import pathlib
 import os.path, time
-import codecs
 import shutil
+from prettytable import PrettyTable
+import glob
 
 
 def write():
     name_file = input('Введите название записи: ')
-    with codecs.open((name_file + '.txt'), 'x', encoding='utf-8') as f:
+    with open((name_file + '.txt'), 'x', encoding='utf-8') as f:
         text_file = input('Введите текст: ')
         f.write(text_file)
   
 
 def data_time():
+    table = PrettyTable()
+    table.field_names = ['Name', 'Date of creation', 'Last change']
+    
     file_path = pathlib.Path().glob('*.txt')
+
     for file in file_path:
         list_files = []
         list_files.extend([file, time.ctime(os.path.getctime(file)), time.ctime(os.path.getmtime(file))])
-        print(*list_files)
-    print('Хотите прочитать запись? [Да/Нет]')
+        table.add_row([*list_files])
+
+    print(table)
+
+    print('Хотите прочитать запись? [Yes/No]')
     n = input('Введите ответ: ')
-    if n == "да":
+    if n == "yes":
         filename = input('Введите название файла, которое хотите прочитать без ".txt": ')
-        with codecs.open((filename + '.txt'), 'r', encoding='utf-8') as f:
+        with open((filename + '.txt'), 'r', encoding='utf-8') as f:
             print(*f.readlines())
     else:
         print('Тоже как вариант...')
         
 
 def path():
-    print('Выберите вариант: 1. Сменить путь 2. Удалить все записи')
-    number_dir = input('Введите номер: ')
-    if number_dir == "1":
-        print('Текущий путь: ', os.path.abspath(__file__))
-        print('Хотите сменить директорию? [Да/Нет]')
-        option_dir = input('Введите ответ: ')
-        if option_dir == "да":
-            new_path = input('Введите новый путь: ')
-            arr = os.listdir()
-            for i in arr:
-                print(i)
-                shutil.move(i, new_path)
-        else:
-            print('Тоже как вариант...')
-    elif number_dir == "2": # не работает
-        k = os.path.abspath(__file__)
-        filelist = [ f for f in k if f.endswith(".txt") ]
-        for f in filelist:
-            os.remove(os.path.join(k, f))
+    print('Текущий путь: ', os.path.abspath(__file__))
+    print('Хотите сменить директорию? [Yes/No]')
+    option_dir = input('Введите ответ: ')
+    if option_dir == "yes":
+        new_path = input('Введите новый путь: ')
+        arr = os.listdir()
+        for i in arr:
+            print(i)
+            shutil.move(i, new_path)
     else:
         print('Тоже как вариант...')
+    
 
 
 def blocknot(choice):
@@ -68,8 +67,8 @@ def blocknot(choice):
         sys.exit()
 
     while True:
-        flag = input('Вернуться в начальное меню? [Да/Нет]: ')
-        if flag == "да":
+        flag = input('Вернуться в начальное меню? [Yes/No]: ')
+        if flag == "yes":
             blocknot(choice)
         else:
             break
